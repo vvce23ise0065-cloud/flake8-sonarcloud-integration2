8cd650fe0ddfa4cfe1153ff56a26ab642332438d
vvce23ise0065-cloud <vvce23ise0065@vvce.ac.in>
2026-05-25 13:28:25 +0530

    trigger github actions

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
 def add(a, b):
-    """Return the sum of two numbers."""
     return a + b
 
-
-result = add(5, 10)
-
-print(result)
-print("Trigger GitHub Action")
+print(add(5, 10))
+print("trigger github action again")
